59d7eec56b014b933ca4f7e61655ab7c49600d44
wesley.msantos9 <wesley.msantos9@CAS554927W10B11.senacsp.edu.br>
2018-08-30 19:07:51 -0300

    Add funcionalidade de deletar no menu.

--- crudProduto/src/main/java/br/senac/pi3/pwda/crudproduto/main/Main.java
+++ crudProduto/src/main/java/br/senac/pi3/pwda/crudproduto/main/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {            
+        
         String tipoProd = null;
         String prod = null;
         ServicoProduto produto = new ServicoProduto();
@@ -17,35 +18,44 @@ public class Main {
         System.out.println("[3] Inserir Produto");
         System.out.println("[4] Atualizar Produto");
         int opc = leitor.nextInt();
-
-        if (opc == 1) {
-            System.out.println("Deseja pesquisar por categoria de produto:");
-            System.out.println("[1] Sim");
-            System.out.println("[2] Não");
-            int opc2 = leitor.nextInt();
-
-            if (opc2 == 1) {
-                System.out.println("Digite a categoria do produto:");
-                tipoProd = leitor.next();
-            } else {
-                tipoProd = "";
-            }
+        
+        switch (opc){
             
-            System.out.println("Deseja visualizar todos o produtos:");
-            System.out.println("[1] Sim");
-            System.out.println("[2] Não");
-            int opc3 = leitor.nextInt();
+            case(1):{
+                System.out.println("Deseja pesquisar por categoria de produto:");
+                System.out.println("[1] Sim");
+                System.out.println("[2] Não");
+                int opc2 = leitor.nextInt();
 
-            if (opc3 == 2) {
-                System.out.print("Digite o nome do produto: ");
-                prod = leitor.next();
-                System.out.print("Digite a descrição do produto: ");
-                
+                if (opc2 == 1) {
+                    System.out.println("Digite a categoria do produto:");
+                    tipoProd = leitor.next();
+                } else {
+                    tipoProd = "";
+                }
+
+                System.out.println("Deseja visualizar todos o produtos:");
+                System.out.println("[1] Sim");
+                System.out.println("[2] Não");
+                int opc3 = leitor.nextInt();
+
+                if (opc3 == 2) {
+                    System.out.print("Digite o nome do produto: ");
+                    prod = leitor.next();
+                    System.out.print("Digite a descrição do produto: ");
+                }
+                produto.select(prod, tipoProd);
             }
-
+                
+            case(2):
+                String nomeProd, retorno;
+                System.out.println("Digite o nome do Produto para ser Deletado: ");
+                nomeProd = leitor.next();
+                retorno = ServicoProduto.remove(nomeProd);
+                if(retorno == null){
+                    System.out.println("Produto excluído com sucesso!");
+                }                
         }
-
-        produto.select(prod, tipoProd);
 
     }
 }
